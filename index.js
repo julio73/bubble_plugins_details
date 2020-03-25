b77e5ca7736ce05bdb84ea8ff893b9b391d94f61
Julio Maniratunga <julio73@users.noreply.github.com>
2020-03-25 18:14:51 +0300

    Verify connection to site

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-index.js
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+rp('https://bubble.io/plugins')
+  .then(content => {
+    console.log(content);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
