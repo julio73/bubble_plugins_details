771d70dd70f0b8bbc2390a3f31e75ba1562b3b36
Julio Maniratunga <julio73@users.noreply.github.com>
2020-03-26 01:58:31 +0300

    Retrieval demo for elements

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('https://bubble.io/plugins');
-  const content = await page.content();
-  console.log(content);
+  await page.goto('https://bubble.io/plugins', {timeout: 0, waitUntil: "networkidle0"});
+
+  const elements = await page.evaluate(() => {
+    return [...document.querySelectorAll('.RepeatingGroup .group-item')].map((el) => {
+      const link = el.querySelector('a.clickable-element');
+      return {name: el.innerText, link: link ? link.getAttribute('href') : null};
+    });
+  });
+  
+  elements.forEach((x) => {console.log(x.name, x.link)});
   await browser.close();
 })();
