8e146a2f508223f8a04e109168bfb7c5c837f186
Julio Maniratunga <julio73@users.noreply.github.com>
2020-03-26 04:22:35 +0300

    Scroll page to load all the elements

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto('https://bubble.io/plugins', {timeout: 0, waitUntil: "networkidle0"});
+  
+  await scrollToBottom(page);
 
   const elements = await page.evaluate(() => {
     return [...document.querySelectorAll('.RepeatingGroup .group-item')].map((el) => {
@@ -15,3 +17,14 @@ const puppeteer = require('puppeteer');
   elements.forEach((x) => {console.log(x.name, x.link)});
   await browser.close();
 })();
+
+async function scrollToBottom(page) {
+  const delay = 400;
+  process.stdout.write('loading');
+  while (await page.evaluate(() => document.scrollingElement.scrollTop + window.innerHeight < document.scrollingElement.scrollHeight)) {
+    await page.evaluate(() => { document.scrollingElement.scrollBy(0, window.innerHeight); });
+    process.stdout.write('.');
+    await page.waitFor(delay);
+  }
+  process.stdout.write('\n');
+}
