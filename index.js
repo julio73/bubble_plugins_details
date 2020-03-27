f9bd05ec09db38bd731f11d2b00a32254fd40afe
Julio Maniratunga <julio73@users.noreply.github.com>
2020-03-27 06:22:44 +0300

    Only retrieve details for plugins with no redirect

--- index.js
+++ index.js
@@ -30,28 +30,30 @@ const cliProgress = require('cli-progress');
   for (let plugin of plugins) {
     let allValues = {...plugin};
     if (/^https:\/\/bubble\.io\/plugin\/.+$/.test(plugin.link)) {
-      await page.goto(plugin.link);
-      // Retrieve plugin details from JSON objects in client db
-      let urlParts = plugin.link.split('-');
-      let pluginId = urlParts[urlParts.length - 1];
-      let pluginData = await page.evaluate((pluginId) => Lib().db_instance()._locals[pluginId].raw, pluginId);
-      
-      if (pluginData) {
-        // Retrieve plugin author details
-        let authorId = pluginData.owner_user != null ? pluginData.owner_user.split('__LOOKUP__')[1] : null;
-        let authorData = authorId ? await page.evaluate((authorId) => Lib().db_instance()._locals[authorId].raw, authorId) : null;
+      const response = await page.goto(plugin.link);
+      if (response.request().redirectChain().length == 0) {
+        // Retrieve plugin details from JSON objects in client db
+        let urlParts = plugin.link.split('-');
+        let pluginId = urlParts[urlParts.length - 1];
+        let pluginData = await page.evaluate((pluginId) => Lib().db_instance()._locals[pluginId].raw, pluginId);
         
-        // Fill in values from corresponding fields
-        allValues.license = pluginData.license_text != null ? pluginData.license_text : '';
-        allValues.pricing =  pluginData.price_number != null ? `${pluginData.price_number}` : '';
-        allValues.one_time_price = pluginData.one_time_price_number != null ? `${pluginData.one_time_price_number}` : '';
-        allValues.usage = `${pluginData.usage_count_number}`;
-        allValues.rating = pluginData.all_versions_rating_number != null ? `${pluginData.all_versions_rating_number}` : '';
-        if (authorData) {
-          allValues.author = `${authorData.plugin_builder_name_text}${authorData.plugin_builder_email_text != null ? ' (' + authorData.plugin_builder_email_text + ')' : ''}`;
+        if (pluginData) {
+          // Retrieve plugin author details
+          let authorId = pluginData.owner_user != null ? pluginData.owner_user.split('__LOOKUP__')[1] : null;
+          let authorData = authorId ? await page.evaluate((authorId) => Lib().db_instance()._locals[authorId].raw, authorId) : null;
+          
+          // Fill in values from corresponding fields
+          allValues.license = pluginData.license_text != null ? pluginData.license_text : '';
+          allValues.pricing =  pluginData.price_number != null ? `${pluginData.price_number}` : '';
+          allValues.one_time_price = pluginData.one_time_price_number != null ? `${pluginData.one_time_price_number}` : '';
+          allValues.usage = `${pluginData.usage_count_number}`;
+          allValues.rating = pluginData.all_versions_rating_number != null ? `${pluginData.all_versions_rating_number}` : '';
+          if (authorData) {
+            allValues.author = `${authorData.plugin_builder_name_text}${authorData.plugin_builder_email_text != null ? ' (' + authorData.plugin_builder_email_text + ')' : ''}`;
+          }
+          allValues.date_created = new Date(pluginData['Created Date']).toUTCString();
+          allValues.date_modified = new Date(pluginData['Modified Date']).toUTCString();
         }
-        allValues.date_created = new Date(pluginData['Created Date']).toUTCString();
-        allValues.date_modified = new Date(pluginData['Modified Date']).toUTCString();
       }
     }
     // Push the values to the table
