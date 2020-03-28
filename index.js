51001ed9f24c4aa1b0d87a3430c319756ab4073c
Julio Maniratunga <julio73@users.noreply.github.com>
2020-03-28 23:58:32 +0300

    Fully switched to intercepting the api call for the plugins

--- index.js
+++ index.js
@@ -1,80 +1,33 @@
 const puppeteer = require('puppeteer');
-const cliProgress = require('cli-progress');
 
 (async () => {
   const browser = await puppeteer.launch({defaultViewport: null});
   const page = await browser.newPage();
-  console.log('Loading plugins page https://bubble.io/plugins ...');
-  await page.goto('https://bubble.io/plugins', {timeout: 0});
   
-  // Scroll through the page to load the plugin list
-  console.log('Scrolling through all plugins...');
-  await scrollToBottom(page);
-
-  // Collect plugins' name and url values
-  const plugins = await page.evaluate(() => {
-    return [...document.querySelectorAll('.RepeatingGroup .group-item')].map((plugin) => {
-      const link = plugin.querySelector('a.clickable-element');
-      return {name: plugin.innerText, link: link ? link.getAttribute('href') : null};
-    });
-  });
-
-  // Build a CSV table
-  let csvTable = [];
-  let csvPrintOrder = ['name','license','pricing','one_time_price','usage','rating','author','date_created','date_modified','link'];
-  csvTable.push(csvPrintOrder.reduce((acc, next) => `${acc}${JSON.stringify(next)},`, ""));
-
-  console.log('Gathering plugin details...');
-  const retrievalProgressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-  retrievalProgressBar.start(plugins.length, 0);
-  for (let plugin of plugins) {
-    let allValues = {...plugin};
-    if (/^https:\/\/bubble\.io\/plugin\/.+$/.test(plugin.link)) {
-      const response = await page.goto(plugin.link);
-      if (response.request().redirectChain().length == 0) {
-        // Retrieve plugin details from JSON objects in client db
-        let urlParts = plugin.link.split('-');
-        let pluginId = urlParts[urlParts.length - 1];
-        let pluginData = await page.evaluate((pluginId) => Lib().db_instance()._locals[pluginId].raw, pluginId);
-        
-        if (pluginData) {
-          // Retrieve plugin author details
-          let authorId = pluginData.owner_user != null ? pluginData.owner_user.split('__LOOKUP__')[1] : null;
-          let authorData = authorId ? await page.evaluate((authorId) => Lib().db_instance()._locals[authorId].raw, authorId) : null;
-          
-          // Fill in values from corresponding fields
-          allValues.license = pluginData.license_text != null ? pluginData.license_text : '';
-          allValues.pricing =  pluginData.price_number != null ? `${pluginData.price_number}` : '';
-          allValues.one_time_price = pluginData.one_time_price_number != null ? `${pluginData.one_time_price_number}` : '';
-          allValues.usage = `${pluginData.usage_count_number}`;
-          allValues.rating = pluginData.all_versions_rating_number != null ? `${pluginData.all_versions_rating_number}` : '';
-          if (authorData) {
-            allValues.author = `${authorData.plugin_builder_name_text}${authorData.plugin_builder_email_text != null ? ' (' + authorData.plugin_builder_email_text + ')' : ''}`;
-          }
-          allValues.date_created = new Date(pluginData['Created Date']).toUTCString();
-          allValues.date_modified = new Date(pluginData['Modified Date']).toUTCString();
-        }
-      }
+  let pluginsResponse = null;
+  page.on('response', async (response) => {
+    const req = response.request();
+    if (req.resourceType() == 'xhr' && req.method() == 'POST' && req.postData().includes('get_integrated_plugins')) {
+      console.log('Intercept plugins request...');
+      pluginsResponse = JSON.parse(await response.text());
     }
-    // Push the values to the table
-    csvTable.push(csvPrintOrder.reduce((acc, next) => `${acc}${JSON.stringify(allValues[next] != null ? allValues[next] : '')},`, ""));
-    retrievalProgressBar.increment();
-  };
-  retrievalProgressBar.stop();
-  console.log('\n\n');
-
-  // Print out the table
-  for (const line of csvTable) {
-    console.log(line);
+  });
+  
+  console.log('Load plugins page https://bubble.io/plugins ...');
+  await page.goto('https://bubble.io/plugins');
+  
+  let allPlugins = []
+  if (pluginsResponse.ret) {
+    console.log('Retrieve JSON response...');
+    allPlugins = pluginsResponse.ret;
+  }
+  
+  console.log('Printout all plugins as csv table...\n\n');
+  let csvPrintOrder = ['name','price','creator_name','creator_id','meta_id'];
+  console.log(csvPrintOrder.reduce((acc, next) => `${acc}${JSON.stringify(next)},`, ""));
+  for (let plugin of allPlugins) {
+    console.log(csvPrintOrder.reduce((acc, next) => `${acc}${JSON.stringify(plugin[next] != null ? plugin[next] : '')},`, ""));
   }
 
   await browser.close();
 })();
-
-async function scrollToBottom(page) {
-  const delay = 400;
-  while (await page.evaluate(() => document.scrollingElement.scrollTop + window.innerHeight < document.scrollingElement.scrollHeight)) {
-    await page.evaluate(() => { document.scrollingElement.scrollBy(0, window.innerHeight); });
-    await page.waitFor(delay);
-  }
-}
